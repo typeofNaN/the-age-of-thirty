a9fa299c45acc0aa4dae530bea912a2b57ebc41a
typeofNaN <dmdefine6@gmail.com>
2026-03-16 22:32:54 +0800

    feat: 优化路由 ✨

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -4,12 +4,16 @@ import { SIDEBAR_GROUP } from './constants'
 export const nav: DefaultTheme.NavItem[] = [
   {
     text: '那些年',
-    link: `${SIDEBAR_GROUP.thoseYears}prologue/old-boy`,
-    activeMatch: SIDEBAR_GROUP.thoseYears
+    activeMatch: SIDEBAR_GROUP.thoseYears,
+    items: [
+      { text: '序章', link: `${SIDEBAR_GROUP.thoseYears}prologue/old-boy` },
+      { text: '你好，世界', link: `${SIDEBAR_GROUP.thoseYears}life/rain` },
+      { text: '读书郎，上学堂', link: `${SIDEBAR_GROUP.thoseYears}study/preschool` }
+    ]
   },
   {
     text: '那些人',
-    link: `${SIDEBAR_GROUP.thosePeople}friend/yong`,
-    activeMatch: SIDEBAR_GROUP.thosePeople
+    activeMatch: SIDEBAR_GROUP.thosePeople,
+    items: [{ text: '那些朋友', link: `${SIDEBAR_GROUP.thosePeople}friend/yong` }]
   }
 ]
--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -2,35 +2,29 @@ import { DefaultTheme } from 'vitepress'
 import { SIDEBAR_GROUP } from './constants'
 
 export const sidebar: DefaultTheme.Sidebar = {
-  [SIDEBAR_GROUP.thoseYears]: {
-    base: '',
-    items: [
-      {
-        text: '序章',
-        items: [{ text: '老男孩', link: `${SIDEBAR_GROUP.thoseYears}prologue/old-boy` }]
-      },
-      {
-        text: '你好，世界',
-        items: [
-          { text: '一场大雨', link: `${SIDEBAR_GROUP.thoseYears}life/rain` },
-          { text: '这是家', link: `${SIDEBAR_GROUP.thoseYears}life/family` },
-          { text: '我的老家，就住在这个屯', link: `${SIDEBAR_GROUP.thoseYears}life/hometown` },
-          { text: '童年', link: `${SIDEBAR_GROUP.thoseYears}life/childhood` }
-        ]
-      },
-      {
-        text: '读书郎，上学堂',
-        items: [{ text: '学前班', link: `${SIDEBAR_GROUP.thoseYears}study/preschool` }]
-      }
-    ]
-  },
-  [SIDEBAR_GROUP.thosePeople]: {
-    base: '',
-    items: [
-      {
-        text: '那些朋友',
-        items: [{ text: '*勇', link: `${SIDEBAR_GROUP.thosePeople}friend/yong` }]
-      }
-    ]
-  }
+  [SIDEBAR_GROUP.thoseYears]: [
+    {
+      text: '序章',
+      items: [{ text: '老男孩', link: `${SIDEBAR_GROUP.thoseYears}prologue/old-boy` }]
+    },
+    {
+      text: '你好，世界',
+      items: [
+        { text: '一场大雨', link: `${SIDEBAR_GROUP.thoseYears}life/rain` },
+        { text: '这是家', link: `${SIDEBAR_GROUP.thoseYears}life/family` },
+        { text: '我的老家，就住在这个屯', link: `${SIDEBAR_GROUP.thoseYears}life/hometown` },
+        { text: '童年', link: `${SIDEBAR_GROUP.thoseYears}life/childhood` }
+      ]
+    },
+    {
+      text: '读书郎，上学堂',
+      items: [{ text: '学前班', link: `${SIDEBAR_GROUP.thoseYears}study/preschool` }]
+    }
+  ],
+  [SIDEBAR_GROUP.thosePeople]: [
+    {
+      text: '那些朋友',
+      items: [{ text: '*勇', link: `${SIDEBAR_GROUP.thosePeople}friend/yong` }]
+    }
+  ]
 }
